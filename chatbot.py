fea02e6cac30d760f205c0a9ec06786dc09c7365
Piotr Kopycki <piokop2@st.amu.edu.pl>
2021-03-21 10:44:19 +0100

    Few pairs for questions added

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,38 @@ pair2 = [
 ]
 
 pair3 = [
-
+    [
+        r"Co (.*)",
+        [
+            "Skąd to pytanie?",
+            "Dlaczego pytasz?",
+            "Nie wiem za bardzo co powiedzieć.",
+            "A co ty o tym myślisz?",
+        ],
+    ],
+    [
+        r"Dlaczego (.*)",
+        [
+            "Prawdę mówiąc nigdy się nie zastanawiałem.",
+            "A co ty o tym myślisz?",
+            "Dlaczego akurat takie pytanie przyszło ci do głowy?",
+        ],
+    ],
+    [
+        r"Czemu (.*)",
+        [
+            "Odpowiedź na to pytanie nie jest prosta.",
+            "Nigdy się nad tym nie zastanawiałem.",
+        ],
+    ],
+    [
+        r"(.*)\?",
+        [
+            "Myślę, że sam znasz odpowiedź na to pytanie.",
+            "Może zastanów się nad tym chwilę.",
+            "Co ty o tym sądzisz?",
+        ],
+    ],
 ]
 
 pair4 = [
